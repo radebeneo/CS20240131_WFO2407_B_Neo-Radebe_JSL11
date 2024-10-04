52a5131ec2a5a0b8e2d69bd69a035ae6103abe15
umkhulubhungane <admin@Apple-MacBook-Pro-Retina-13-Early-2015.local>
2024-10-04 16:14:41 +0200

    task details, status.

--- index.js
+++ index.js
@@ -234,6 +234,7 @@ function openEditTaskModal(task) {
   // Set task details in modal inputs
   document.getElementById('edit-task-title-input').value = task.title;
   document.getElementById('edit-task-desc-input').value = task.description;
+  document.getElementById('edit-select-status').value = task.status;
 
   // Get button elements from the task modal
 
